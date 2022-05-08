56f5a5612eab186123fbd2c29129e0518477b599
wa5ina <ennasry.rida@gmail.com>
2022-05-08 17:22:03 +0100

    hopefully no more recoding from scratch

--- mandatory/srcs/lexer/lexer.c
+++ mandatory/srcs/lexer/lexer.c
@@ -6,54 +6,16 @@
 /*   By: ren-nasr <ren-nasr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 20:38:43 by ren-nasr          #+#    #+#             */
-/*   Updated: 2022/05/05 12:42:59 by ren-nasr         ###   ########.fr       */
+/*   Updated: 2022/05/08 17:20:06 by ren-nasr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+// if you find a single quote , 
 
 #include <readline/history.h>
 #include <readline/readline.h>
-// state machine for lexer
-// - state 0:
-//     - if char is a space, go to state 0 
-
-/* Backus Naur Form of shell grammar
-
-    cmd [arg]* [ | cmd [arg]*]* 
-        [[>filename][<filename][>&filename][>>filename][>>&filename]]*[&]
-
-    cmd:
-        [a-zA-Z0-9_]+ 
-    arg:
-        [a-zA-Z0-9_]+
-    filename:
-        [a-zA-Z0-9_]+
-    &:
-        &
-    >:
-        >
-    <:
-        <
-    >>:
-        >>
-    >>&:
-        >>&
-    [:
-        [
-    ]:
-        ]
-        
-*/
 
 
-// lex and tokenize a string 
-// return a list of tokens
-
-// state machine :
-// - cmd state 
-// - 
-// ======= pseudo code for lexer =======
     
 
 // the lexer will take the given string and create tokens out if it.
@@ -62,36 +24,13 @@
 
 
 // the values and their tokens are: 
-// -> cmd: <cmd cmd flags>
+// -> cmd: <cmd flags>
 
 // get the part after <cmd and before >
-
-
-// <
-// <in> <cmd ls -al> <pipe> <cmd cat> <pipe> <cmd wc -la> <out> 
-
-
-    // state machine :
-    
-        // printable is not an operator.
-                //  handle which operator.
-        // printable is a space.
-                // 
-        // printable is a letter.
-    
-
-        // - if char is a '>', go to state 0
-        // - if char is a '<', go to state 1
-        // - if char is a '|', go to state 2
-        // - if char is a quote go to state 3
-        // - if char is a '$', go to state 4
-        // - else char * is a cmd to state 5
-        
-
+ 
 
 
 #include "../../includes/minishell.h"
-
 /* 
     -> analyzing the problem : 
         - the lexer will take the given string and create tokens out of it.
@@ -123,22 +62,10 @@
          
 */
 
-#define CMD_LEN 128
-t_cmd cmds[CMD_LEN];
-
-
-
-int isOperator(char c, char c1)
-{
-    return (0);
-}
-
-
-
 
 // the lexer is a state machine :
 
-bool cmd_exist(char *cmd)
+char *cmd_exist(char *cmd)
 {
 
     char *PATH = getenv("PATH");
@@ -148,10 +75,10 @@ bool cmd_exist(char *cmd)
     if (ft_strchr(cmd, '/'))
     {
         if (access(cmd, F_OK) == 0)
-            return (true);
+            return (cmd);
         else
             return (false);
-    }
+    } 
     while (paths[i])
     {
         char *path = paths[i];
@@ -159,8 +86,7 @@ bool cmd_exist(char *cmd)
         cmd_path = ft_strjoin(cmd_path, cmd);
         if (access(cmd_path, F_OK) == 0)
         {
-            return (true);
-            
+            return (cmd_path);
         }
         i++;
     }
@@ -168,84 +94,230 @@ bool cmd_exist(char *cmd)
 }
 
 
-// int handle_cmd(char *argv)
-// {
-//     int j;
-//     j = 0;
-//     char *cmd_tmp;
-//     while (argv[j]) {            
-//     if (isOperator(argv[j]))
-//          break;
-//         cmd_tmp[j] = argv[j];
-//     }
-//     exitIF(cmd_exist(cmd_tmp) == false, "command not found");
-//     // add the cmd to the cmds array
-    
-// }
-
-
-void lexer(char *str)
+t_data *init_data(t_data *data)
 {
-        t_data *data = malloc(sizeof(*data));
-        data->cmds = malloc(sizeof(*cmds)); 
+    data = malloc(sizeof(*data));
+    // cmd linked list : 
+    data->cmds = malloc(sizeof(*data->cmds)); 
+    data->cmds->next = NULL;
+    
+    data->tokens = malloc(sizeof(*data->tokens));
+    data->env = malloc(sizeof(*data->env));
+    data->env->index = 0;
+    data->env->env = malloc(sizeof(*data->env->env));
+    exitIF(!data || !data->cmds || !data->tokens || !data->env \
+        || !data->env->env, "allocation failed");
+    return (data);
+}
+
+int    skip_space(char *s, int *i)
+{
+    int index = *i;
+    while (ft_isspace(s[index]))
+        index++;
+    *i = index;
+    return index;
+}
+
+int getState(char c, char c1)
+{
+    if (ft_isspace(c))
+        return (0);
+    else if (ft_isalnum(c))
+        return (1);
+    else if (c == '\'' | c == '"')
+        return (2);
+    else if (c == '<' && c1 == '<')
+        return (3);
+    else if (c == '>' && c1 == '>')
+        return (4);
+    else if (c == '<')
+        return (5);
+    else if (c == '>')
+        return (6);
+    else if (c == '|' && c1 == '|')
+        return (7);
+    else if (c == '|')
+        return (8);
+    else if (c == '&' && c1 == '&')
+        return (9);
+    else if (c == '$' && c1 == '?')
+        return (10);
+    else if (c == '$')
+        return (11);
+    else 
+        return (12);
+}
+
+
+
+void    handle_cmd(t_data *data, char *line, int *i)
+{
+    /*
+        Pseudo code :
+            -> get cmd.
+                -> if cmd is valid.
+                    -> if flag is set.
+                        -> get flag.
+                            -> if flag is valid.
+                                -> store cmd.
+                                -> store flag.
+                                -> tokonize the cmd and flag.
+                    -> else:
+                        -> error.
+                        -> exit.
+                    -> else:
+                        -> store cmd.
+                        -> tokonize the cmd.
+                -> else:
+                    -> error.
+                    -> exit.
+    */
+   // move to the last node of the cmd linked list.
+   
+    t_cmd *tmp = malloc(sizeof(*tmp));
+    t_cmd *current;
+    int index = *i;
+     
+
+    tmp->cmd = ft_substr(line, index, index + (ft_strchr(&line[index], ' ') - line - index));
+    
+    // check if cmd is valid
+    
+    tmp->cmd = cmd_exist(tmp->cmd);
+    if (!tmp->cmd)
+        exitFree(data, "command not found");
+    current = data->cmds;
+    while (current->next)
+        current = current->next;
+    current = tmp;
+    current->next = NULL;
+    // tokonize the cmd
+    data->tokens = ft_strjoin(data->tokens, "<cmd> ");
+    index += (ft_strchr(&line[index], ' ') - line - index);
+    // printf("%s\n", current->cmd);
+    // printf("before : %d\n", index);
+    index = skip_space(line, &index);
+    // printf("%d, after\n", index);
+    
+    // check if flag is set 
+    if (line[index] == '-' || getState(line[index], line[index + 1]) == 1)
+    {
+        // get flag 
+        current->args = malloc(sizeof(*tmp->args) * 2);
+        if (!current->args)
+            exitFree(data, "allocation failed");
+        current->args[0] = ft_substr(line, index, index + (ft_strchr(&line[index], ' ') - line - index));
+        current->args[1] = NULL;
+        index += (ft_strchr(&line[index], ' ') - line - index);
+    }
+    else
+    {
+        current->args = malloc(sizeof(*tmp->args) * 1);
+        exitFree(data, "allocation failed");
+        current->args[0] = NULL;
+    }
+    if (current->cmd)
+        printf("%s\n", current->cmd);
+    if (current->args[0])
+        printf("%s\n", current->args[0]);
+    if (data->tokens)
+        printf("%s\n", data->tokens);
+    *i = index;
+    
+}
+// <in> <cmd ls -al> <pipe> <cmd cat> <pipe> <cmd wc -la> <out> 
+
+        // if char is a space go to state 0
+        // if char is alphanumeric go to state 1
+        // if char is a '\'' or '"' go to state 2
         
-        char **tmp = ft_splitq(str, ' ');
-        int i = 0 ;
-        while (tmp[i])
+        // - if char is a '>', go to state 3
+        // - if char is a '<', go to state 4
+        // - if char is a '|', go to state 5
+        // - if char is a quote go to state 6
+        // - if char is a '$', go to state 8
+
+
+
+void lexer(char *line)
+{
+    
+    t_data *data = NULL;
+    data = init_data(data);
+    
+    int i = 0;
+    while (line[i])
+    {
+        if (getState(line[i], line[i + 1]) == 0)
         {
-            if (!strncmp(tmp[i], "<", 1))
-            {
-                exitIF(!tmp[i + 1] || access(tmp[i + 1], F_OK) == -1, "file not found"); 
-                data->in = ft_strdup(tmp[i + 1]);
-                // data->tokens =  ft_strjoin(data->tokens, IN);
-                // data->tokens = ft_strjoin(data->tokens, " ");
-                i++;
-                
-                // check if the file exist
-                
-               // we assume that next char is a file name.
-                    // - we check if the file exist.
-                    // - if it exist. 
-                        // - we tokenize the in redirection. 
-                        // add the infile to the data instance.
-                    // - else :
-                        // - error.
-                        // - exit.
-                
-                
-                
-            }
-            else if (!strncmp(tmp[i], ">", 1))
-            {
-                printf("outfile detected\n");
-            }
-            else if (!strncmp(tmp[i], "|", 1))
-            {
-                printf("pipe detected\n");
-            }
-            else if (!strncmp(tmp[i], "\"", 1))
-            {
-                printf("quote detected\n");
-            }
-            else if (!strncmp(tmp[i], "$", 1))
-            {
-                printf("env detected\n");
-            }
-            else
-            {
-                printf("cmd detected\n");
-            }
-           
-            i++;
+            skip_space(line, &i);
         }
-                
+        else if (getState(line[i], line[i + 1]) == 1)
+        {
+            // don't forget to handle flag.
+             handle_cmd(data, line, &i);
+        }
+        else if (getState(line[i], line[i + 1]) == 2)
+        {
+            // handle quote, state 2
+            
+            
+        }
+        else if (getState(line[i], line[i + 1]) == 3)
+        {
+            // handle >> , state 3
+            
+            
+        }
+        else if (getState(line[i], line[i + 1] == 4))
+        {
+            // handle << , state 4 , what's called heredoc
+           
+            // data->tokens = ft_strjoin(data->tokens, "<hd> ");
+           
+            
+        }
+        else if (getState(line[i], line[i + 1] == 5))
+        {
+            // handle < , state 5
+        }
+        else if (getState(line[i], line[i + 1] == 6))
+        {
+            // handle > , state 6
+        }
+        else if (getState(line[i], line[i + 1]) == 7)
+        {
+            // handle || , state 7
+        }
+        else if (getState(line[i], line[i + 1] == 8))
+        {
+            // handle | , state 7
+        }  
+        else if (getState(line[i], line[i + 1] == 9))
+        {
+            // hanle state && , state 8
+        }
+        
+        else if (getState(line[i], line[i + 1] == 10))
+        {
+            // handle $? , state 10
+            
+        }
+        else if (getState(line[i], line[i + 1] == 11))
+        {
+            // handle $ , state 11
+        }
+        else 
+            i++;
+    }
 }
 
 
 
 
 
-int main(int argc, char **argv)
+int main(void)
 {
     // lexer(argc, argv);
     // print all arguments
